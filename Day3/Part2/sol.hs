170074cd2ebffc6b01b385dd96e570b3fc0285ac
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2021-12-22 01:16:39 +0530

    day 2 part 2

--- Day3/Part2/sol.hs
+++ Day3/Part2/sol.hs
@@ -3,35 +3,56 @@ import Control.Applicative
 import Control.Monad
 import Data.Maybe
 import System.IO (isEOF)
+import Debug.Trace
 
--- 'Arrow' is not a good name for part 2
-data Arrow = Forward Integer | Up Integer | Down Integer deriving (Read, Show)
-
-parseInput :: [String] -> [Arrow]
-parseInput [] = []
-parseInput (x:xs)
-  | first == "forward" = (Forward secondInt) : parseInput xs
-  | first == "up"      = (Up secondInt) : parseInput xs
-  | first == "down"    = (Down secondInt) : parseInput xs
-    where (first:second:[]) = words x
-          secondInt = read second :: Integer
+parseInput :: [String] -> [String]
+parseInput = id
 
 readLines :: IO [String]
 readLines = do
   line <- getLine
   done <- isEOF
   if done
-     then return [line]
-     else (line :) <$> readLines
+    then return [line]
+    else (line :) <$> readLines
 
-depthProduct :: [Arrow] -> Integer
-depthProduct = depthProductAcum 0 0 0 
+mostCommonBit :: [String] -> Int -> Char
+mostCommonBit x k = if count >= (((length x) + 1) `div` 2) then '1' else '0'
   where 
-    depthProductAcum x y _ [] = x * y
-    depthProductAcum x y z ((Forward u):us) = depthProductAcum (x + u) (y + z * u) z us
-    depthProductAcum x y z ((Up u):us)      = depthProductAcum x y (z - u) us
-    depthProductAcum x y z ((Down u):us)    = depthProductAcum x y (z + u) us
+    count = fst $ foldl fun (0, k) x
+    fun (currentCount, k) x
+      | x !! k == '1' = (currentCount + 1, k)
+      | otherwise = (currentCount, k)
+
+filterWithMcb :: [String] -> Int -> Char -> [String]
+filterWithMcb [] _ _ = []
+filterWithMcb (x:xs) k mcb
+  | (x !! k) == mcb = (x:) $ filterWithMcb xs k mcb
+  | otherwise = filterWithMcb xs k mcb
+
+filterBits :: [String] -> Int -> Int -> String
+filterBits x k n  = if (n <= k || (length x) == 1) then head x else filterBits (filterWithMcb x k mcb) (k + 1) n
+  where
+    mcb = mostCommonBit x k
+
+filterBits' :: [String] -> Int -> Int -> String
+filterBits' x k n  = if (n <= k || (length x) == 1) then head x else filterBits' (filterWithMcb x k mcb) (k + 1) n
+  where
+    mcb = if mostCommonBit x k == '1' then '0' else '1'
+
+binToDec :: [Int] -> Int
+binToDec = foldl fun 0
+  where
+    fun n x = x + (2 * n)
+
+charToInt :: String -> [Int]
+charToInt = map (\a -> if a == '1' then 1 else 0)
+
+complement :: [Int] -> [Int]
+complement = map (\a -> if a == 1 then 0 else 1) 
 
 main = do
   lines <- readLines
-  print $ (depthProduct . parseInput) lines
+  let gamma = charToInt $ filterBits (parseInput lines) 0 (length $ head lines)
+      epsilon = charToInt $ filterBits' (parseInput lines) 0 (length $ head lines)
+  print (binToDec gamma * binToDec epsilon)  
